10de857b7f3e9c65989e13a48bf7911490651401
tomdaniels <thomsondaniels@hotmail.com>
2018-05-15 16:57:21 +1000

    push id's on post method

--- src/middleware/plex-requests-controllers.js
+++ src/middleware/plex-requests-controllers.js
@@ -10,6 +10,7 @@ exports.listMovieId = function(req, res) {
 
 exports.addMovieId = function(req, res) {
   const message = `the movie that has been requested: ${req.params.id}`;
+  database.ref('movies').push(req.params.id);
   res.send(message);
   console.log(message);
 }
@@ -20,6 +21,7 @@ exports.listTvId = function(req, res) {
 
 exports.addTvId = function(req, res) {
   const message = `the show that has been requested: ${req.params.id}`;
+  database.ref('tv').push(req.params.id);
   res.send(message);
   console.log(message);
 }
@@ -30,6 +32,10 @@ exports.listTvSeasonId = function(req, res) {
 
 exports.addTvSeasonId = function(req, res) {
   const message = `the show ID that has been requested: ${req.params.id} (SeasonID): ${req.params.seasonId}`;
+  database.ref('tv/seasons').push({
+    show: req.params.id,
+    season: req.params.seasonId,
+  });
   res.send(message);
   console.log(message);
 }
