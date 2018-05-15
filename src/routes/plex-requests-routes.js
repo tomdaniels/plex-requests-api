90427f97828ad6dc4109d3c56d85dda5a9968e54
tomdaniels <thomsondaniels@hotmail.com>
2018-05-15 17:49:52 +1000

    remove requests client host from accepted origin list

--- src/routes/plex-requests-routes.js
+++ src/routes/plex-requests-routes.js
@@ -4,7 +4,6 @@ module.exports = function(app) {
 
   app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
-    res.setHeader('Access-Control-Allow-Origin', 'http://requests.tomd.io');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
