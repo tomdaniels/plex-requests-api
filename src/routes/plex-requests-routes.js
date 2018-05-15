01a26031dbc85f9209bf69082be65e477bc2d183
tomdaniels <thomsondaniels@hotmail.com>
2018-05-15 18:01:13 +1000

    fix allowed sites declaration

--- src/routes/plex-requests-routes.js
+++ src/routes/plex-requests-routes.js
@@ -3,7 +3,7 @@ module.exports = function(app) {
   const prefix = '/v1';
 
   app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.setHeader('Access-Control-Allow-Origin', 'http://requests.tomd.io');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
