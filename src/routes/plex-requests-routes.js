ae67a8dbfaf6a83c5de83ddaa608dfcada92f8bc
tomdaniels <thomsondaniels@hotmail.com>
2018-06-12 12:40:42 +1000

    refactor: routes.js

--- src/routes/plex-requests-routes.js
+++ src/routes/plex-requests-routes.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
 
   app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'http://requests.tomd.io');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
     next();
