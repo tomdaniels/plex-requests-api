8be282f3caeffd8e9b484ae495594677b1ec8587
tomdaniels <thomsondaniels@hotmail.com>
2018-06-12 13:02:25 +1000

    fix routes filename

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = require('./src/routes/plex-requests-routes');
+const router = require('./src/routes/routes');
 
 const app = express();
 const port = process.env.PORT || 3000;
