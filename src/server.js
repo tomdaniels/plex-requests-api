0fec95648da032d14f19bc002570991aae31d608
tomdaniels <thomsondaniels@hotmail.com>
2018-08-02 19:20:19 +1000

    fix: remove redundant config

--- src/server.js
+++ src/server.js
@@ -1,21 +1,9 @@
-let newRelic;
-
-if (process.env.NODE_ENV === 'production') {
-  // eslint-disable-next-line global-require
-  newRelic = require('newrelic');
-}
-
 const config = require('config');
 const createLogger = require('./core/logger');
 const start = require('./app');
 
 start().then((app) => {
   const logger = createLogger();
-
-  if (config.isProduction) {
-    app.locals.newrelic = newRelic; // eslint-disable-line no-param-reassign
-  }
-
   const port = config.get('PORT');
 
   app.listen(port, () => {
