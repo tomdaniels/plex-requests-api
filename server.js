d1dbadc8e205422f4f33d0feecbec56a06146c6c
tomdaniels <thomsondaniels@hotmail.com>
2018-08-02 12:14:12 +1000

    docs: setup swagger

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = require('./src/routes/routes');
+const configureSwaggerWithMetrics = require('./src/middleware/swagger-with-metrics');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 router(app);
+app.use(configureSwaggerWithMetrics);
 
 app.listen(port);
 
