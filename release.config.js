befccce57cfc27e833cefa628e10bbbe2f618382
tomdaniels <thomsondaniels@hotmail.com>
2019-03-07 10:22:13 +1100

    feat: semantic-release req'd config :lock:

---
+++ release.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  branch: 'demo',
+  dryRun: false,
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          { type: 'feat', release: 'patch' },
+          { type: 'fix', release: 'patch' },
+          { type: 'docs', release: 'patch' },
+          { type: 'style', release: 'patch' },
+          { type: 'perf', release: 'patch' },
+          { type: 'ci', release: 'patch' },
+          { type: 'chore', release: 'patch' },
+        ],
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogTitle: '# Changelog',
+      },
+    ],
+    '@semantic-release/npm',
+    '@semantic-release/git',
+    '@semantic-release/github',
+  ],
+};
